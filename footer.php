41752b126417a37f80301750b6bc4bb054692a4c
LebonMatthieu <matthieu.lebon93@gmail.com>
2015-01-28 12:02:41 +0100

    modif footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 </div>
-<div id="footer" class="largeur-de-la-balle">Le footer</div>
+<div id="footer" class="largeur-de-la-balle">By twa kiltir johnfordman | sayrobert | judpom2 | LapOuD' @hetic Web1</div>
 </body>
 </html>
