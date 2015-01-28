5b93e8a94cafb621a8c7e14de38a9f17eb475c7a
Johnfordman <ndoulou.jean@gmail.com>
2015-01-28 12:01:45 +0100

    ajout de luffy
    luff the boss

--- anime.php
+++ anime.php
@@ -1,5 +1,6 @@
 	<h1>Animes!</h1>
 	<p>
 		Les animes c'est trop fort!</p>
-	<img src="images/Fd.jpg" width="192" height="120"><br/>
-	<img src="images/key_art_mushi_shi.jpg"><br/>
+	<img src="images/Fd.jpg" width="192" height="120">
+	<img src="images/key_art_mushi_shi.jpg">
+	<img src="images/luffy.jpg" alt="">
